71b810cbb1229d4c6e1581b305e0a48e831a7859
truart <trubinart@gmail.com>
2021-05-12 22:14:23 +0300

    hw 3 done

--- job_sites/class_modul.py
+++ job_sites/class_modul.py
@@ -16,20 +16,20 @@ class Vacancy:
     def get_salary_hh(self, salary_string):
         if salary_string.find('от') != -1:
             split_string = salary_string.replace('\u202f', '').split(' ')
-            self.min_salary = split_string[1]
+            self.min_salary = int(split_string[1])
             self.maх_salary = None
             self.currency = split_string[2].replace('.', '')
 
         elif salary_string.find('до') != -1:
             split_string = salary_string.replace('\u202f', '').split(' ')
             self.min_salary = None
-            self.maх_salary = split_string[1]
+            self.maх_salary = int(split_string[1])
             self.currency = split_string[2].replace('.', '')
 
         else:
             split_string = salary_string.replace('\u202f', '').split(' ')
-            self.min_salary = split_string[0]
-            self.maх_salary = split_string[2]
+            self.min_salary = int(split_string[0])
+            self.maх_salary = int(split_string[2])
             self.currency = split_string[3].replace('.', '')
 
     def get_salary_superjob(self, salary_string):
@@ -40,26 +40,26 @@ class Vacancy:
 
         elif salary_string.find('от') != -1:
             split_string = salary_string.split('\xa0')
-            self.min_salary = split_string[1] + split_string[2]
+            self.min_salary = int(split_string[1] + split_string[2])
             self.maх_salary = None
             self.currency = split_string[3].replace('.', '')
 
         elif salary_string.find('до') != -1:
             split_string = salary_string.split('\xa0')
             self.min_salary = None
-            self.maх_salary = split_string[1] + split_string[2]
+            self.maх_salary = int(split_string[1] + split_string[2])
             self.currency = split_string[3].replace('.', '')
 
         elif len(salary_string.split('\xa0')) == 3:
             split_string = salary_string.split('\xa0')
-            self.min_salary = split_string[0] + split_string[1]
+            self.min_salary = int(split_string[0] + split_string[1])
             self.maх_salary = None
             self.currency = split_string[2].replace('.', '')
 
         else:
             split_string = salary_string.split('\xa0')
-            self.min_salary = split_string[0] + split_string[1]
-            self.maх_salary = split_string[3] + split_string[4]
+            self.min_salary = int(split_string[0]) + int(split_string[1])
+            self.maх_salary = int(split_string[3]) + int(split_string[4])
             self.currency = split_string[5].replace('.', '')
 
     def __str__(self):
@@ -78,7 +78,7 @@ class VacancyList:
         self.vacancy_text = vacancy_text
         self.page_count = page
         self.add_hh()
-        self.add_superjob()
+        # self.add_superjob()
 
     def add_vacancy(self, vacancy):
         self.all_vacancy.append(vacancy)
@@ -98,25 +98,29 @@ class VacancyList:
             params['page'] = page_number
             response = requests.get(domain + url, params=params, headers=self.headers)
             dom = bs(response.text, 'html.parser')
+            is_final_page = dom.find('div', {'data-qa': 'bloko-header-1'})
 
-            card_list = dom.find_all('div', {'class': 'vacancy-serp-item'})
+            if is_final_page is None:
+                card_list = dom.find_all('div', {'class': 'vacancy-serp-item'})
 
-            for card in card_list:
-                vacancy = Vacancy()
-                vacancy.name = card.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).text
-                vacancy.domain = domain
-                vacancy.link = card.find('a', {'data-qa': 'vacancy-serp__vacancy-title'})['href']
+                for card in card_list:
+                    vacancy = Vacancy()
+                    vacancy.name = card.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).text
+                    vacancy.domain = domain
+                    vacancy.link = card.find('a', {'data-qa': 'vacancy-serp__vacancy-title'})['href']
 
-                try:
-                    salary = card.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text
-                    vacancy.get_salary_hh(salary)
+                    try:
+                        salary = card.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text
+                        vacancy.get_salary_hh(salary)
 
-                except  AttributeError:
-                    vacancy.min_salary = None
-                    vacancy.maх_salary = None
-                    vacancy.currency = None
+                    except  AttributeError:
+                        vacancy.min_salary = None
+                        vacancy.maх_salary = None
+                        vacancy.currency = None
 
-                self.all_vacancy.append(vacancy)
+                    self.all_vacancy.append(vacancy)
+            else:
+                break
 
     def add_superjob(self):
         domain = 'https://www.superjob.ru'
@@ -130,31 +134,38 @@ class VacancyList:
             params['page'] = page_number
             response = requests.get(domain + url, params=params, headers=self.headers)
             dom = bs(response.text, 'html.parser')
+            is_final_page = dom.find(text='Ничего не нашлось')
 
-            card_list = dom.find_all('div', {'class': re.compile('f-test-vacancy-item')})
-            for card in card_list:
-                vacancy = Vacancy()
-                try:
-                    vacancy.name = card.find('span', {'class': '_1rS-s'}).text \
-                                   + card.find('span', {'class': '_1rS-s'}).next_sibling
-                    vacancy.domain = domain
-                    vacancy.link = domain + card.find('span', {'class': '_1rS-s'}).parent['href']
+            if is_final_page is None:
+                card_list = dom.find_all('div', {'class': re.compile('f-test-search-result-item')})
+                for card in card_list:
+                    vacancy = Vacancy()
+                    try:
+                        vacancy.name = card.find('span', {'class': '_1rS-s'}).text \
+                                       + card.find('span', {'class': '_1rS-s'}).next_sibling
+                        print(f'{vacancy.name} + {page_number}')
+                        print(card)
+                        vacancy.domain = domain
+                        vacancy.link = domain + card.find('span', {'class': '_1rS-s'}).parent['href']
+                        print(f'{vacancy.name} + {page_number} + {vacancy.link}')
 
-                except  AttributeError:
-                    vacancy.name = card.find('a', {'class': re.compile('f-test-link')}).text
-                    vacancy.domain = domain
-                    vacancy.link = domain + card.find('a', {'class': re.compile('f-test-link')})['href']
+                    except  AttributeError:
+                        vacancy.name = card.find('a', {'class': re.compile('f-test-link')}).text
+                        vacancy.domain = domain
+                        vacancy.link = domain + card.find('a', {'class': re.compile('f-test-link')})['href']
 
-                try:
-                    salary = card.find('span', {'class': 'f-test-text-company-item-salary'}).next.text
-                    vacancy.get_salary_superjob(salary)
+                    try:
+                        salary = card.find('span', {'class': 'f-test-text-company-item-salary'}).next.text
+                        vacancy.get_salary_superjob(salary)
 
-                except  AttributeError:
-                    vacancy.min_salary = None
-                    vacancy.maх_salary = None
-                    vacancy.currency = None
+                    except  AttributeError:
+                        vacancy.min_salary = None
+                        vacancy.maх_salary = None
+                        vacancy.currency = None
 
-                self.all_vacancy.append(vacancy)
+                    self.all_vacancy.append(vacancy)
+            else:
+                break
 
     def __str__(self):
         return f'Все вакансии {self.all_vacancy}'
--- job_sites/get_resume.py
+++ job_sites/get_resume.py
@@ -1,15 +1,58 @@
 from class_modul import VacancyList
 import gc
+from pymongo import MongoClient
+import hashlib
+import json
+from pprint import pprint
+from function_module import vacancy_filter_by_salary
 
-final_list = VacancyList(vacancy_text='web', page=2)
+# ЗАПИСЬ В ФАЙЛ
+# final_list = VacancyList(vacancy_text='web', page=2)
+#
+# with open('parser_results.txt', 'w') as file:
+#     file.write(f'Было обработано {final_list.page_count} страниц \n'
+#                f'Всего вакансий: {final_list.vacancy_count}\n'
+#                f'{"-" * 20}\n')
+#
+#     for vacancy in final_list.all_vacancy:
+#         file.write(f'{vacancy.domain} | {vacancy.name} | {vacancy.min_salary}-{vacancy.maх_salary}-{vacancy.currency}\n'
+#                    f'{vacancy.link}\n'
+#                    f'{"-" * 20}\n')
+# gc.collect()
 
-with open('parser_results.txt', 'w') as file:
-    file.write(f'Было обработано {final_list.page_count} страниц \n'
-               f'Всего вакансий: {final_list.vacancy_count}\n'
-               f'{"-" * 20}\n')
 
-    for vacancy in final_list.all_vacancy:
-        file.write(f'{vacancy.domain} | {vacancy.name} | {vacancy.min_salary}-{vacancy.maх_salary}-{vacancy.currency}\n'
-                   f'{vacancy.link}\n'
-                   f'{"-" * 20}\n')
-gc.collect()
+# ЗАПИСЬ В MONGODB
+client = MongoClient('localhost', 27017)
+db = client['vacancy_db']
+
+try:
+    collection = db.create_collection('vacancy')
+except:
+    collection = db.vacancy
+
+final_list = VacancyList(vacancy_text='повар', page=1)
+
+for vacancy in final_list.all_vacancy:
+    vacancy_for_mongo = {
+        'name': vacancy.name,
+        'domain': vacancy.domain,
+        'link': vacancy.link,
+        'min_salary': vacancy.min_salary,
+        'maх_salary': vacancy.maх_salary,
+        'currency': vacancy.currency,
+    }
+    vacancy_for_mongo_binary = json.dumps(vacancy_for_mongo, ).encode('utf-8')
+    hash = hashlib.sha3_256(vacancy_for_mongo_binary)
+    id = hash.hexdigest()
+
+    vacancy_for_mongo['_id'] = id
+
+    try:
+        collection.insert_one(vacancy_for_mongo)
+    except Exception:
+        continue
+
+for doc in collection.find({}):
+    pprint(doc)
+
+vacancy_filter_by_salary(150000, collection)
